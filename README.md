9fccca30d06012f865e9e1c738c19f23532d02d6
Sebastián Zaffarano <sebas@zaffarano.com.ar>
2021-11-21 16:24:36 +0100

    First implementation

---
+++ README.md
@@ -0,0 +1,64 @@
+## TDA
+
+Helper command inspired in [daycaptain tools](https://github.com/daycaptain/tools/) implemented in golang using just 
+stdlib, without any third party dependencies.
+
+The goal is to support many architectures without the need to install dependencies.
+
+It's in an early stage of development, so can have some bugs, and definitely some missing features.
+
+### Usage
+
+```sh
+$ go build ./...
+```
+
+```sh
+$ ./tda -h
+expected task name, got: []
+
+Usage: tda [options] <task name>
+
+Token can be either specified via the -token flag, via the $DC_API_TOKEN 
+environment variable, or via the $DC_API_TOKEN_COMMAND environment variable.
+The last option is useful when the token is stored in a command line tool, e.g.
+
+export DC_API_TOKEN_COMMAND="pass some/key"
+
+Options:
+  -W	the task to this week
+  -d string
+    	Add the task to the DATE (formatted by ISO-8601, e.g. 2021-01-31)
+  -date string
+    	Add the task to the DATE (formatted by ISO-8601, e.g. 2021-01-31)
+  -i	(Default)  Add the task to the backlog inbox (default true)
+  -inbox
+    	(Default)  Add the task to the backlog inbox (default true)
+  -m	Add the task to tomorrow's tasks
+  -t	Add the task to today's tasks
+  -today
+    	Add the task to today's tasks
+  -token string
+    	API key, default $DC_API_TOKEN or $DC_API_TOKEN_COMMAND
+  -tomorrow
+    	Add the task to tomorrow's tasks
+  -version
+    	Prints current version and exit
+  -w string
+    	Add the task to the WEEK (formatted by ISO-8601, e.g. 2021-W07)
+  -week string
+    	Add the task to the WEEK (formatted by ISO-8601, e.g. 2021-W07)
+```
+
+Instead of hard-code the API KEY, you can use the environment variable $DC_API_TOKEN_COMMAND to store the key in a 
+command line tool.
+
+```sh
+export DC_API_TOKEN_COMMAND="pass some/key"
+```
+
+### TODO
+
+- [ ] Organize the code (i.e. move the functions to a separate file).
+- [ ] Add code coverage.
+- [ ] Add git workflow to publish relaases.
--- cmd/tda/tda.go
+++ cmd/tda/tda.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"os/exec"
+	"regexp"
 	"strings"
+	"time"
 )
 
 const (
 	// DayCaptainURL is the base URL for the DayCaptain API
-	DayCaptainURL = "https://daycaptain.com/api/"
+	DayCaptainURL = "https://daycaptain.com"
 
 	// TokenEnvVar is the environment variable name for the token
 	TokenEnvVar = "DC_API_TOKEN"
+
+	// TokenCmdEnvVar environment variable that holds a command to run to get the token
+	TokenCmdEnvVar = "DC_API_TOKEN_COMMAND"
 )
 
 var (
 	token   string
 	version bool
-	debug   bool
+
+	tdaConfig TdaConfig
 )
 
 var (
@@ -26,11 +37,94 @@ var (
 	Version = "dev"
 )
 
+type Task struct {
+	String string `json:"string"`
+}
+
+type TdaConfig struct {
+	today    bool
+	tomorrow bool
+	date     string
+	thisWeek bool
+	week     string
+	inbox    bool
+}
+
+type Request struct {
+	url string
+}
+
+func (c *TdaConfig) GetCommand() (Request, error) {
+	optionsSet := make([]Request, 0)
+	fallback := Request{fmt.Sprintf("%s/backlog-items", DayCaptainURL)}
+
+	if c.today {
+		now := time.Now().Format("2006-01-02")
+		optionsSet = append(optionsSet, Request{fmt.Sprintf("%s/%s/tasks", DayCaptainURL, now)})
+	}
+
+	if c.tomorrow {
+		tomorrow := time.Now().AddDate(0, 0, 1).Format("2006-01-02")
+		optionsSet = append(optionsSet, Request{fmt.Sprintf("%s/%s/tasks", DayCaptainURL, tomorrow)})
+	}
+
+	if c.date != "" {
+		current, err := time.Parse("2006-01-02", c.date)
+		if err != nil {
+			return Request{}, err
+		}
+		optionsSet = append(optionsSet, Request{fmt.Sprintf("%s/%s/tasks", DayCaptainURL, current.Format("2006-01-02"))})
+	}
+
+	if c.thisWeek {
+		year, isoWeek := time.Now().ISOWeek()
+		optionsSet = append(optionsSet, Request{fmt.Sprintf("%s/%d-W%d/tasks", DayCaptainURL, year, isoWeek)})
+	}
+
+	if c.week != "" {
+		r, err := regexp.Compile(`^\d{4}-W\d{2}$`)
+		if err != nil {
+			panic(err)
+		}
+
+		if !r.Match([]byte(c.week)) {
+			return Request{}, fmt.Errorf("invalid week format: %s", c.week)
+		}
+
+		optionsSet = append(optionsSet, Request{fmt.Sprintf("%s/%s/tasks", DayCaptainURL, c.week)})
+	}
+
+	switch len(optionsSet) {
+	case 0:
+		return fallback, nil
+	case 1:
+		return optionsSet[0], nil
+	default:
+		return Request{}, fmt.Errorf("More than one option specified: %v", optionsSet)
+	}
+}
+
 func init() {
 	// initialize the flags
-	flag.StringVar(&token, "token", "", "token")
-	flag.BoolVar(&version, "version", false, "version")
-	flag.BoolVar(&debug, "debug", false, "debug")
+	flag.StringVar(&token, "token", "", "API key, default $DC_API_TOKEN or $DC_API_TOKEN_COMMAND")
+	flag.BoolVar(&version, "version", false, "Prints current version and exit")
+
+	flag.BoolVar(&tdaConfig.today, "t", false, "Add the task to today's tasks")
+	flag.BoolVar(&tdaConfig.today, "today", false, "Add the task to today's tasks")
+
+	flag.BoolVar(&tdaConfig.tomorrow, "m", false, "Add the task to tomorrow's tasks")
+	flag.BoolVar(&tdaConfig.tomorrow, "tomorrow", false, "Add the task to tomorrow's tasks")
+
+	flag.StringVar(&tdaConfig.date, "d", "", "Add the task to the DATE (formatted by ISO-8601, e.g. 2021-01-31)")
+	flag.StringVar(&tdaConfig.date, "date", "", "Add the task to the DATE (formatted by ISO-8601, e.g. 2021-01-31)")
+
+	flag.BoolVar(&tdaConfig.thisWeek, "W", false, "the task to this week")
+
+	flag.StringVar(&tdaConfig.week, "w", "", "Add the task to the WEEK (formatted by ISO-8601, e.g. 2021-W07)")
+	flag.StringVar(&tdaConfig.week, "week", "", "Add the task to the WEEK (formatted by ISO-8601, e.g. 2021-W07)")
+
+	flag.BoolVar(&tdaConfig.inbox, "i", true, "(Default)  Add the task to the backlog inbox")
+	flag.BoolVar(&tdaConfig.inbox, "inbox", true, "(Default)  Add the task to the backlog inbox")
 }
 
 func main() {
@@ -42,19 +136,58 @@ func main() {
 		return
 	}
 
-	token = fromEnv(TokenEnvVar, token)
+	token = fromEnv(TokenEnvVar, TokenCmdEnvVar, token)
 	if token == "" {
 		usage("Token is mandatory")
 		return
 	}
 
 	if len(flag.Args()) != 1 {
-		usage(fmt.Sprintf("%v: expected one single command", strings.Join(flag.Args(), ", ")))
+		usage(fmt.Sprintf("expected task name, got: %q", flag.Args()))
 		return
 	}
 
-	fmt.Println(flag.Arg(0))
-	fmt.Println(token)
+	cmd, err := tdaConfig.GetCommand()
+	if err != nil {
+		usage(err.Error())
+		return
+	}
+
+	body, err := json.Marshal(Task{String: flag.Arg(0)})
+	if err != nil {
+		usage(err.Error())
+		return
+	}
+
+	req, err := http.NewRequest("POST", cmd.url, bytes.NewReader(body))
+	if err != nil {
+		usage(err.Error())
+		return
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	req.Header.Add("Content-Type", "application/json")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		usage(err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			usage(err.Error())
+			return
+		}
+
+		usage(fmt.Sprintf("%s: %s", resp.Status, string(body)))
+		return
+	}
+
+	fmt.Println("OK")
 }
 
 func printVersion() {
@@ -65,11 +198,19 @@ func help() {
 	usage("")
 }
 
-func fromEnv(name, fallback string) string {
-	if value, ok := os.LookupEnv(name); ok {
+func fromEnv(name, nameCmd, value string) string {
+	if value != "" {
 		return value
+	} else if envValue, ok := os.LookupEnv(name); ok {
+		return envValue
+	} else if cmd, ok := os.LookupEnv(nameCmd); ok {
+		command := strings.Split(cmd, " ")
+		if output, err := exec.Command(command[0], command[1:]...).Output(); err == nil {
+			// remove trailing newline
+			return strings.Replace(string(output), "\n", "", 1)
+		}
 	}
-	return fallback
+	return value
 }
 
 func usage(msg string) {
@@ -77,21 +218,18 @@ func usage(msg string) {
 		fmt.Fprintf(os.Stderr, msg)
 		fmt.Fprintf(os.Stderr, "\n\n")
 	}
-	fmt.Fprintf(os.Stderr, "usage: tda [options] [COMMAND]\n")
-	fmt.Fprintf(os.Stderr, "Flags:\n")
-	flag.PrintDefaults()
-	fmt.Fprintf(os.Stderr, commands())
-	os.Exit(2)
-}
+	header := `Usage: tda [options] <task name>
 
-func commands() string {
-	return `
-  Commands:
-  date      Add the task to the DATE (formatted by ISO-8601, e.g. 2021-01-31
-  inbox     Add the task to the backlog inbox
-  today     Add the task to today's tasks
-  tomorrow  Add the task to tomorrow's tasks
-  w         Add the task to this week
-  week      Add the task to the WEEK
-  `
+Token can be either specified via the -token flag, via the $DC_API_TOKEN 
+environment variable, or via the $DC_API_TOKEN_COMMAND environment variable.
+The last option is useful when the token is stored in a command line tool, e.g.
+
+export DC_API_TOKEN_COMMAND="pass some/key"
+
+Options:
+`
+
+	fmt.Fprintf(os.Stderr, header)
+	flag.PrintDefaults()
+	os.Exit(2)
 }
